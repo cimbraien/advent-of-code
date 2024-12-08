c9b29e794d46984b653464e55bc5d1527392f56b
Kimbrian Marshall <cimbraien@gmail.com>
2024-12-09 04:36:21 +0700

    2015: 4

---
+++ 2015/4b.ts
@@ -0,0 +1,12 @@
+const input = "iwrupvqb";
+
+function solution(input: string): number {
+  let i = 0;
+  while (true) {
+    const hash = new Bun.CryptoHasher("md5").update(input + i.toString()).digest("hex");
+    if (hash.startsWith("000000")) return i;
+    i++;
+  }
+}
+
+console.log(solution(input));
